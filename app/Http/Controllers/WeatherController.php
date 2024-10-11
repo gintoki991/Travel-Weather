bdab1627b38bccc792c7a36847fae880995f6544
gintoki <midoir041206@gmail.com>
2024-10-11 21:46:10 +0900

    API呼び出しにオプションを追加

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -15,14 +15,20 @@ class WeatherController extends Controller
     }
 
     // 都市名で現在の天気情報を取得するメソッド
-    public function getWeatherByCityName($cityName)
+    public function getWeatherByCityName($cityName, Request $request)
     {
         $coordinates = $this->weatherService->getCoordinates($cityName);
 
         if (!empty($coordinates)) {
             $latitude = $coordinates[0]['lat'];
             $longitude = $coordinates[0]['lon'];
-            $weatherData = $this->weatherService->getCurrentWeather($latitude, $longitude);
+
+            // リクエストからオプションパラメータを取得（デフォルトは摂氏と日本語）
+            $units = $request->get('units', 'metric');
+            $lang = $request->get('lang', 'ja');
+
+            $weatherData = $this->weatherService->getCurrentWeather($latitude,$longitude, $units, $lang);
+
             return response()->json($weatherData);
         } else {
             return response()->json(['error' => 'Location not found'], 404);
@@ -30,15 +36,25 @@ class WeatherController extends Controller
     }
 
     // 都市名で5日間の天気予報を取得するメソッド
-    public function getForecastByCityName($cityName)
+    public function getForecastByCityName($cityName, Request $request)
     {
         $coordinates = $this->weatherService->getCoordinates($cityName);
 
         if (!empty($coordinates)) {
             $latitude = $coordinates[0]['lat'];
             $longitude = $coordinates[0]['lon'];
-            $forecastData = $this->weatherService->getForecast($latitude, $longitude);
-            return response()->json($forecastData);
+
+            $units = $request->get('units', 'metric');
+            $lang = $request->get('lang', 'ja');
+            $cnt = $request->get('cnt', null);
+
+            $forecastData = $this->weatherService->getForecast($latitude, $longitude, $units, $lang, $cnt);
+
+            if ($forecastData) {
+                return response()->json($forecastData);
+            } else {
+                return response()->json(['error' => 'Failed to retrieve forecast data'], 500);
+            }
         } else {
             return response()->json(['error' => 'Location not found'], 404);
         }
--- app/Services/WeatherService.php
+++ app/Services/WeatherService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class WeatherService
 {
@@ -30,28 +31,47 @@ class WeatherService
   }
 
   // 現在の天気を取得するメソッド
-  public function getCurrentWeather($latitude, $longitude)
+  public function getCurrentWeather($latitude, $longitude, $units = 'metric', $lang = 'ja')
   {
     $response = Http::get($this->currentWeatherUrl, [
       'lat' => $latitude,
       'lon' => $longitude,
       'appid' => $this->apiKey,
-      'units' => 'metric', // 摂氏表示のため
+      'units' => $units,
+      'lang' => $lang,
     ]);
 
     return $response->json();
   }
 
   // 5日間の天気予報を取得するメソッド
-  public function getForecast($latitude, $longitude)
+  public function getForecast($latitude, $longitude, $units = 'metric', $lang = 'ja', $cnt = null)
   {
-    $response = Http::get($this->forecastUrl, [
-      'lat' => $latitude,
-      'lon' => $longitude,
-      'appid' => $this->apiKey,
-      'units' => 'metric', // 摂氏表示のため
-    ]);
+    try {
+      $params = [
+        'lat' => $latitude,
+        'lon' => $longitude,
+        'appid' => $this->apiKey,
+        'units' => $units,
+        'lang' => $lang,
+      ];
 
-    return $response->json();
+      if ($cnt) {
+        $params['cnt'] = $cnt;
+      }
+
+      $response = Http::get($this->forecastUrl, $params);
+
+      if ($response->successful()) {
+        return $response->json();
+      } else {
+        // エラーレスポンスの内容をログに出力
+        Log::error('Forecast API error: ' . $response->body());
+        return null;
+      }
+    } catch (\Exception $e) {
+      Log::error('Forecast API exception: ' . $e->getMessage());
+      return null;
+    }
   }
 }
