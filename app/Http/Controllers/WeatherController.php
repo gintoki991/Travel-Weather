3eede71b0ceb4061d0e096cbdd961bd21cae82a3
gintoki <midoir041206@gmail.com>
2024-10-17 10:22:04 +0900

    WeatherController で WeatherRequest クラスを使用するように変更

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,29 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\WeatherRequest;
 use App\Services\WeatherService;
 
 class WeatherController extends Controller
 {
     protected $weatherService;
-    protected $cityName;
-    protected $selectedDate;
-    protected $weatherData;
 
     public function __construct(WeatherService $weatherService)
     {
         $this->weatherService = $weatherService;
     }
 
-    // 都市名で現在の天気情報と5日間の天気予報を取得するメソッド
-    public function getWeatherByCityName($cityName, Request $request)
+    /**
+     * 都市名で現在の天気情報と5日間の天気予報を取得するメソッド
+     *
+     * @param WeatherRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getWeatherByCityName(WeatherRequest $request)
     {
-        $units = $request->get('units', 'metric');
-        $lang = $request->get('lang', 'ja');
+        $units = 'metric';
+        $lang = 'ja';
 
-        // WeatherServiceの共通メソッドを利用して天気データを取得
-        $weatherData = $this->weatherService->fetchWeatherData($cityName, $units, $lang);
+        $weatherData = $this->weatherService->fetchWeatherData($request->city, $units, $lang);
 
         if ($weatherData) {
             return response()->json($weatherData);
@@ -33,18 +34,17 @@ class WeatherController extends Controller
         }
     }
 
-    // 都市名と日付で天気情報と指数を取得するメソッド
-    public function getWeatherData(Request $request)
+    /**
+     * 都市名と日付で天気情報と指数を取得するメソッド
+     *
+     * @param WeatherRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function getWeatherData(WeatherRequest $request)
     {
-        $cityName = $request->get('city');
-        $selectedDate = $request->get('date', now()->toDateString()); // 日付がなければ今日の日付
+        $selectedDate = now()->toDateString(); // サーバー側で日付を設定
 
-        if (empty($cityName)) {
-            return response()->json(['error' => 'City name is required'], 400);
-        }
-
-        // WeatherServiceを使って天気データを取得
-        $forecast = $this->weatherService->fetchWeatherData($cityName, 'metric', 'ja', $selectedDate);
+        $forecast = $this->weatherService->fetchWeatherData($request->city, 'metric', 'ja', $selectedDate);
 
         if (!$forecast) {
             return response()->json(['error' => 'Weather data not found'], 404);
@@ -59,7 +59,6 @@ class WeatherController extends Controller
             'indexes' => $indexes
         ];
 
-        // JSONでレスポンスを返す
         return response()->json($weatherData);
     }
 }
